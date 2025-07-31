2e9f636e6b976d15d684fbf87c9a3d8c3701c365
bala21072004 <bala.subbramani2107@gmail.com>
2025-07-31 11:43:36 +0530

    js basics

---
+++ practice1.js
@@ -0,0 +1,17 @@
+let array=["1,2,4,6,7,9"];
+console.log(array[2]);
+array[14]=42;
+console.log(array);
+console.log(array.length)
+
+let a=12;
+let b="null"
+let c;
+let d=true;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+let details={Name:"bala",course:"Bsc",subject:"english", mark:97};
+console.log(details);
