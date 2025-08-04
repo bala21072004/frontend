2d4922ad653a2229d3c8bd99673116dc8ba89f33
bala21072004 <bala.subbramani2107@gmail.com>
2025-08-04 16:29:38 +0530

    Add files via upload

---
+++ oprators.js
@@ -0,0 +1,22 @@
+// Relastional Operators
+let a = 10;
+let b = 5;
+console.log(a > b);   // true
+console.log(a < b);   // false
+console.log(a == b);  // false
+console.log(a != b);  // true
+console.log(a === 10); // true 
+console.log( a >= b);   // true
+console.log( a <= b);   // true 
+
+//arithmetic operators
+let c = 10;
+let d = 3;
+console.log( c + d);  // 13
+console.log( c - d);  // 7
+console.log( c * d);  // 30
+console.log( c / d);  // 3.333...
+console.log( c % d);  // 1 
+
+
+
